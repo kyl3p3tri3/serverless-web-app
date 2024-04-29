2002da7b166753e842ec96527e1e8986f340da9a
kyl3p3tri3 <kyl3p3tri3@gmail.com>
2024-04-29 11:00:23 -0500

    added config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-1_KnpidF1i9',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '6hvdgjat89gir5o7lbbto2u6r9',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://85c3n8y1ck.execute-api.us-east-1.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
